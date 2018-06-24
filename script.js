92e2071acf9d7d59ad16cc73254f076223b2d437
mayank31793 <mayank31793@gmail.com>
2018-06-24 15:15:44 +0530

    json file added

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    data = JSON.parse(this.responseText);
+    console.log(typeof data);
+    console.log(data[0].techno[0].videotitle);
+    console.log(data[0].people[0].videotitle);
+  }
+};
+xhttp.open("GET", "videodata.json", true);
+xhttp.send();
+
+document.getElementById('video1').addEventListener('click',function(){
+  document.getElementById('modalBox').setAttribute('src',data[0].techno[0].link);
+  document.getElementsByClassName('modal-title')[0].innerHTML = data[0].techno[0].videotitle;
+});
+
+document.getElementById('video2').addEventListener('click',function(){
+  document.getElementById('modalBox').setAttribute('src',data[0].techno[1].link);
+  document.getElementsByClassName('modal-title')[0].innerHTML = data[0].techno[1].videotitle;
+});
+
+document.getElementById('video3').addEventListener('click',function(){
+  document.getElementById('modalBox').setAttribute('src',data[0].techno[2].link);
+  document.getElementsByClassName('modal-title')[0].innerHTML = data[0].techno[2].videotitle;
+});
+
+document.getElementById('closeModal').addEventListener('click',function(){
+  document.getElementById('modalBox').setAttribute('src','');
+});
+
 var topvideocontainer = document.getElementById('topVideos');
 var mainVideo = document.getElementsByClassName('big')[0];
 var miniVideo1 = document.getElementsByClassName('mini')[0];
@@ -5,11 +36,7 @@ var miniVideo2 = document.getElementsByClassName('mini')[1];
 
 topvideocontainer.style.height = '400px';
 
-console.log(topvideocontainer.offsetHeight);
-
 mainVideo.style.height = topvideocontainer.offsetHeight+'px';
-
-console.log(mainVideo.style.height);
 
 miniVideo1.style.height = (topvideocontainer.offsetHeight)/2+'px';
 
@@ -20,3 +47,10 @@ if(window.innerWidth < 768){
 	miniVideo1.style.height = '130px';
 	miniVideo2.style.height = '130px';
 }
+
+var techvideodiv = document.getElementsByClassName('technoVideo');
+
+var f = document.getElementById('footer');
+var example = document.createElement('button');
+
+example.setAttribute('class','btn btn-dark');
