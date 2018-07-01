fa4e091039a270c82d9a326e5c34184938f984b8
mayank31793 <mayank31793@gmail.com>
2018-07-01 17:27:01 +0530

    Carousel square thumbnail

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+pathFile = 'videodata.json';
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -6,153 +8,62 @@ xhttp.onreadystatechange = function() {
     console.log(data[0].techno[0].videotitle);
     console.log(data[0].people[0].videotitle);
 
-  // for(var i=0;i<data[0].techno.length;i++){
-  //   var item = document.createElement('div');
-  //   item.setAttribute('class','col=xl-4 col-lg-4 col-md-4 col-sm-4 col-4 d-flex justify-content-center technoVideo');
-
-  //   var itemImage = document.createElement('img');
-  //   itemImage.setAttribute('src',data[0].techno[i].thumbnail);
-  //   itemImage.setAttribute('class','thumbnail');
-
-  //   item.appendChild(itemImage);
-  //   technoRow.appendChild(item);
-  // }
-
   var divCarousel,imgCarousel;
 
-  for(var i=0;i<data[0].techno.length;i++){
-
-    if((i)%3 == 0){
+  function categories(a,category,parentElement){
+    if((a)%3 == 0){
 
       divCarousel = document.createElement('div');
       divCarousel.setAttribute('class','carousel-item');
-      divCarousel.setAttribute('id',i);      
+      divCarousel.setAttribute('id',a);      
 
       imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',data[0].techno[i].thumbnail);
-      imgCarousel.setAttribute('id',i);
-      imgCarousel.style.width = '32vw';
-      imgCarousel.style.height = '220px'; 
+      imgCarousel.setAttribute('src',category);
+      imgCarousel.setAttribute('id',a);
+      imgCarousel.setAttribute('class','col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4');
+      imgCarousel.style.width = '';
+      imgCarousel.style.height = ''; 
 
       divCarousel.appendChild(imgCarousel);
-      technoCarouselDiv.appendChild(divCarousel);
-      console.log('inside techno multiple 3',data[0].techno[i].thumbnail);
+      parentElement.appendChild(divCarousel);
+      console.log('inside techno multiple 3',category);
 
     }
 
     else{
       imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',data[0].techno[i].thumbnail);
-      imgCarousel.setAttribute('id',i);
-      imgCarousel.style.width = '32vw';
-      imgCarousel.style.height = '220px';
+      imgCarousel.setAttribute('src',category);
+      imgCarousel.setAttribute('id',a);
+      imgCarousel.setAttribute('class','col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4');
+      imgCarousel.style.width = '';
+      imgCarousel.style.height = '';
 
       divCarousel.appendChild(imgCarousel);
-      console.log('inside techno not multiple 3',data[0].techno[i].thumbnail);
+      console.log('inside techno not multiple 3',category);
     }
 
-    if(i==0){
+    if(a==0){
       divCarousel.setAttribute('class','carousel-item active');
     }
+  }
 
+
+  for(var i=0;i<data[0].techno.length;i++){
+    categories(i,data[0].techno[i].thumbnail,technoCarouselDiv);
   }  
 
   for(var i=0;i<data[0].people.length;i++){
-    // var item = document.createElement('div');
-    // item.setAttribute('class','col=xl-4 col-lg-4 col-md-4 col-sm-4 col-4 d-flex justify-content-center technoVideo');
-
-    // var itemImage = document.createElement('img');
-    // itemImage.setAttribute('src',data[0].people[i].thumbnail);
-    // itemImage.setAttribute('class','thumbnail');
-
-    // item.appendChild(itemImage);
-    // peopleRow.appendChild(item);
-
-    if((i)%3 == 0){
-
-      divCarousel = document.createElement('div');
-      divCarousel.setAttribute('class','carousel-item');
-      divCarousel.setAttribute('id',i); 
-
-      imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',data[0].people[i].thumbnail);
-      imgCarousel.setAttribute('id',i);
-      imgCarousel.style.width = '32vw';
-      imgCarousel.style.height = '220px';      
-
-      divCarousel.appendChild(imgCarousel);
-      peopleCarouselDiv.appendChild(divCarousel);
-      console.log('inside techno multiple 3',data[0].people[i].thumbnail);
-
-    }
-
-    else{
-      imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',data[0].people[i].thumbnail);
-      imgCarousel.setAttribute('id',i);
-      imgCarousel.style.width = '32vw';
-      imgCarousel.style.height = '220px';
-
-      divCarousel.appendChild(imgCarousel);
-      console.log('inside techno not multiple 3',data[0].people[i].thumbnail);
-    }
-
-    if(i==0){
-      divCarousel.setAttribute('class','carousel-item active');
-    }
-
+    categories(i,data[0].people[i].thumbnail,peopleCarouselDiv);
   }
 
   for(var i=0;i<data[0].environment.length;i++){
-    // var item = document.createElement('div');
-    // item.setAttribute('class','col=xl-4 col-lg-4 col-md-4 col-sm-4 col-4 d-flex justify-content-center technoVideo');
-
-    // var itemImage = document.createElement('img');
-    // itemImage.setAttribute('src',data[0].environment[i].thumbnail);
-    // itemImage.setAttribute('class','thumbnail');
-
-    // item.appendChild(itemImage);
-    // environmentRow.appendChild(item);
-
-    if((i)%3 == 0){
-
-      divCarousel = document.createElement('div');
-      divCarousel.setAttribute('class','carousel-item');
-      divCarousel.setAttribute('id',i); 
-
-      imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',data[0].environment[i].thumbnail);
-      imgCarousel.setAttribute('id',i);
-      imgCarousel.style.width = '32%';
-      imgCarousel.style.height = '220px';
-
-      divCarousel.appendChild(imgCarousel);
-      environmentCarouselDiv.appendChild(divCarousel);
-      console.log('inside techno multiple 3',data[0].environment[i].thumbnail);
-
-    }
-
-    else{
-      imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',data[0].environment[i].thumbnail);
-      imgCarousel.setAttribute('id',i);
-      imgCarousel.style.width = '32vw';
-      imgCarousel.style.height = '220px';
-
-      divCarousel.appendChild(imgCarousel);
-      console.log('inside techno not multiple 3',data[0].environment[i].thumbnail);
-    }
-
-    if(i==0){
-      divCarousel.setAttribute('class','carousel-item active');
-    }
-
+    categories(i,data[0].environment[i].thumbnail,environmentCarouselDiv);
   }    
 
   }
 };
 
-xhttp.open("GET", "videodata.json", true);
+xhttp.open("GET", pathFile, true);
 xhttp.send();
 
 document.getElementById('video1').addEventListener('click',function(){
@@ -218,5 +129,9 @@ document.getElementById('english').addEventListener('click',function(){
 });
 
 document.getElementById('hindi').addEventListener('click',function(){
-  document.getElementById('language').innerHTML = 'हिन्दी';  
+  document.getElementById('language').innerHTML = 'हिन्दी'; 
+  pathFile = 'videodatahindi.json';
+  location.reload();
 });
+
+console.log(pathFile);
