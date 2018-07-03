30f93ed7421ac59c381dab0c3d3db1203106f4ec
mayank31793 <mayank31793@gmail.com>
2018-07-03 19:30:20 +0530

    Iframe working fine

--- script.js
+++ script.js
@@ -1,96 +1,99 @@
-pathFile = 'videodata.json';
+// pathFile = 'videodata.json';
 
-  var divCarousel,imgCarousel;
+var divCarousel,imgCarousel;
 
-  function categories(a,category,parentElement,iframesrc){
-    if((a)%3 == 0){
+function categories(a,category,parentElement,field){
+  if((a)%3 == 0){
 
-      divCarousel = document.createElement('div');
-      divCarousel.setAttribute('class','carousel-item');
-      // divCarousel.setAttribute('id','div'+a);      
+    divCarousel = document.createElement('div');
+    divCarousel.setAttribute('class','carousel-item');
+    // divCarousel.setAttribute('id','div'+a);      
 
-      imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',category);
-      imgCarousel.setAttribute('id',a);
-      imgCarousel.setAttribute('class','col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4');
-      imgCarousel.style.width = '';
-      imgCarousel.style.height = ''; 
+    imgCarousel = document.createElement('img');
+    imgCarousel.setAttribute('src',category);
+    imgCarousel.setAttribute('id',field+a);
+    imgCarousel.setAttribute('class','col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4');
+    imgCarousel.style.width = '';
+    imgCarousel.style.height = ''; 
 
-      divCarousel.appendChild(imgCarousel);
-      parentElement.appendChild(divCarousel);
-
-    }
-
-    else{
-      imgCarousel = document.createElement('img');
-      imgCarousel.setAttribute('src',category);
-      imgCarousel.setAttribute('id',a);
-      imgCarousel.setAttribute('class','col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4');
-      imgCarousel.style.width = '';
-      imgCarousel.style.height = '';
-
-      divCarousel.appendChild(imgCarousel);
-
-    }
-
-    if(a==0){
-      divCarousel.setAttribute('class','carousel-item active');  
-
-    }
+    divCarousel.appendChild(imgCarousel);
+    parentElement.appendChild(divCarousel);
 
   }
 
-var xhttp = new XMLHttpRequest();
+  else{
+    imgCarousel = document.createElement('img');
+    imgCarousel.setAttribute('src',category);
+    imgCarousel.setAttribute('id',field+a);
+    imgCarousel.setAttribute('class','col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4');
+    imgCarousel.style.width = '';
+    imgCarousel.style.height = '';
+
+    divCarousel.appendChild(imgCarousel);
+
+  }
+
+  if(a==0){
+    divCarousel.setAttribute('class','carousel-item active');  
+
+  }
+
+}
+
+xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     data = JSON.parse(this.responseText);
-    console.log(typeof data);
+    console.log(typeof data);        
 
   for(var i=0;i<data[0].techno.length;i++){
-    categories(i,data[0].techno[i].thumbnail,technoCarouselDiv); 
-    document.getElementById(i).addEventListener('click',function(){
-      var xyz = this;
-      console.log(xyz.id);
+    categories(i,data[0].techno[i].thumbnail,technoCarouselDiv,Object.keys(data[0])[0]); 
+    document.getElementById(Object.keys(data[0])[0]+i).addEventListener('click',function(e){
+      var strget = e.target.id;
+      var strpos = strget.charAt(6);
+      var numstrpos = parseInt(strpos);
       document.getElementById('clickedTechnoVid').style.display = 'block';
-      document.getElementById('clickedTechnoIframe').setAttribute('src',data[0].techno[this.id].link);
-      document.getElementById('clickedTechnoIframeHeading').innerHTML = data[0].techno[this.id].videotitle;
-      document.getElementById('clickedTechnoIframeDescription').innerHTML = data[0].techno[this.id].description;
+      document.getElementById('clickedTechnoIframe').setAttribute('src',data[0].techno[numstrpos].link);
+      document.getElementById('clickedTechnoIframeHeading').innerHTML = data[0].techno[numstrpos].videotitle;
+      document.getElementById('clickedTechnoIframeDescription').innerHTML = data[0].techno[numstrpos].description;
     });       
   }  
 
   for(var j=0;j<data[0].people.length;j++){
-    categories(j,data[0].people[j].thumbnail,peopleCarouselDiv);
-    document.getElementById(j).addEventListener('click',function(){
-      var abc = this;
-      console.log(abc);
+    categories(j,data[0].people[j].thumbnail,peopleCarouselDiv,Object.keys(data[0])[1]);
+    document.getElementById(Object.keys(data[0])[1]+j).addEventListener('click',function(e){
+      var strget = e.target.id;
+      var strpos = strget.charAt(6);
+      var numstrpos = parseInt(strpos);      
       document.getElementById('clickedPeopleVid').style.display = 'block';
-      document.getElementById('clickedPeopleIframe').setAttribute('src',data[0].people[this.id].link);
-      document.getElementById('clickedPeopleIframeHeading').innerHTML = data[0].people[this.id].videotitle;
-      document.getElementById('clickedPeopleIframeDescription').innerHTML = data[0].people[this.id].description;
+      document.getElementById('clickedPeopleIframe').setAttribute('src',data[0].people[numstrpos].link);
+      document.getElementById('clickedPeopleIframeHeading').innerHTML = data[0].people[numstrpos].videotitle;
+      document.getElementById('clickedPeopleIframeDescription').innerHTML = data[0].people[numstrpos].description;
     });    
   }
 
   for(var k=0;k<data[0].environment.length;k++){
-    categories(k,data[0].environment[k].thumbnail,environmentCarouselDiv);
-    document.getElementById(k).addEventListener('click',function(){
-      var def = this;
-      console.log(def.id);
+    categories(k,data[0].environment[k].thumbnail,environmentCarouselDiv,Object.keys(data[0])[2]);
+    document.getElementById(Object.keys(data[0])[2]+k).addEventListener('click',function(e){
+      var strget = e.target.id;
+      var strpos = strget.charAt(11);
+      var numstrpos = parseInt(strpos);      
       document.getElementById('clickedEnvironmentVid').style.display = 'block';
-      document.getElementById('clickedEnvironmentIframe').setAttribute('src',data[0].environment[this.id].link);
-      document.getElementById('clickedEnvironmentIframeHeading').innerHTML = data[0].environment[this.id].videotitle;
-      document.getElementById('clickedEnvironmentIframeDescription').innerHTML = data[0].environment[this.id].description;
+      document.getElementById('clickedEnvironmentIframe').setAttribute('src',data[0].environment[numstrpos].link);
+      document.getElementById('clickedEnvironmentIframeHeading').innerHTML = data[0].environment[numstrpos].videotitle;
+      document.getElementById('clickedEnvironmentIframeDescription').innerHTML = data[0].environment[numstrpos].description;
     });    
   }    
 
   }
 };
 
-xhttp.open("GET", pathFile, true);
+xhttp.open("GET",'videodata.json', true);
 xhttp.send();
 
-// document.getElementById('clickedTechnoVid').style.display = 'none';
-// document.getElementById('clickedPeopleVid').style.display = 'none';
-// document.getElementById('clickedEnvironmentVid').style.display = 'none';
+document.getElementById('clickedTechnoVid').style.display = 'none';
+document.getElementById('clickedPeopleVid').style.display = 'none';
+document.getElementById('clickedEnvironmentVid').style.display = 'none';
 
 document.getElementById('video1').addEventListener('click',function(){
   document.getElementById('modalBox').setAttribute('src',data[0].techno[0].link);
@@ -148,13 +151,14 @@ document.getElementById('closeLanguageBox').addEventListener('click',function(){
   document.getElementById('languagesAvailable').style.display = 'none';
 });
 
-document.getElementById('english').addEventListener('click',function(){
-  document.getElementById('language').innerHTML = 'English';  
-});
+// document.getElementById('english').addEventListener('click',function(){
+//   document.getElementById('language').innerHTML = 'English';  
+// });
 
-document.getElementById('hindi').addEventListener('click',function(){
-  document.getElementById('language').innerHTML = 'हिन्दी'; 
-});
+// document.getElementById('hindi').addEventListener('click',function(){
+//   document.getElementById('language').innerHTML = 'हिन्दी'; 
+//   console.log('Hindi');
+// });
 
 document.getElementById('clickedTechnoIframeclose').addEventListener('click',function(){
   document.getElementById('clickedTechnoVid').style.display = 'none';
@@ -170,5 +174,3 @@ document.getElementById('clickedEnvironmentIframeclose').addEventListener('click
   document.getElementById('clickedEnvironmentVid').style.display = 'none';
   document.getElementById('clickedEnvironmentIframe').setAttribute('src','');
 });
-
-console.log(pathFile);
